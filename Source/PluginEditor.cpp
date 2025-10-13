5acb75d823bb162636954efbddeba9bf79c9511a
mrboyd78 <mr.christopherboyd@gmail.com>
2025-10-13 17:48:13 -0500

    chore: Remove duplicate source files

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,59 +1,32 @@
+#include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-VstTestPlaygroundAudioProcessorEditor::VstTestPlaygroundAudioProcessorEditor(VstTestPlaygroundAudioProcessor& p)
-    : AudioProcessorEditor(&p), processorRef(p)
+//==============================================================================
+VstTestPlaygroundAudioProcessorEditor::VstTestPlaygroundAudioProcessorEditor (VstTestPlaygroundAudioProcessor& p)
+    : AudioProcessorEditor (&p), processorRef (p)
 {
-    // Instantiate the WebBrowserComponent
-    webBrowserComponent = std::make_unique<juce::WebBrowserComponent>();
+    gainSlider.setSliderStyle (juce::Slider::Rotary);
+    gainSlider.setTextBoxStyle (juce::Slider::NoTextBox, false, 90, 0);
+    gainSlider.setPopupDisplayEnabled (true, false, this);
+    gainSlider.setTextValueSuffix (" dB");
+    addAndMakeVisible (&gainSlider);
 
-    // Configure the options as per the guide's recommendations
-    auto options = juce::WebBrowserComponent::Options()
-        .withBackend(juce::WebBrowserComponent::Options::Backend::webview2)
-        .withWinWebView2Options(
-            juce::WebBrowserComponent::Options::WinWebView2{}
-                .withUserDataFolder(juce::File::getSpecialLocation(juce::File::tempDirectory)
-                    .getChildFile("VstTestPlayground_" + juce::String(juce::Process::getCurrentProcessId())))
-        )
-        .withNativeIntegrationEnabled()
-        .withKeepPageLoadedWhenBrowserIsHidden();
+    gainAttachment.reset (new juce::AudioProcessorValueTreeState::SliderAttachment (processorRef.apvts, "gain", gainSlider));
 
-    webBrowserComponent->setOptions(options);
-
-    // Create relays and attachments for parameter synchronization
-    gainRelay = std::make_unique<juce::WebSliderRelay>(*webBrowserComponent, "GAIN");
-    gainAttachment = std::make_unique<juce::WebSliderParameterAttachment>(
-        *processorRef.apvts.getParameter("GAIN"), *gainRelay, nullptr);
-
-    onOffRelay = std::make_unique<juce::WebSliderRelay>(*webBrowserComponent, "ONOFF");
-    onOffAttachment = std::make_unique<juce::WebSliderParameterAttachment>(
-        *processorRef.apvts.getParameter("ONOFF"), *onOffRelay, nullptr);
-
-    addAndMakeVisible(*webBrowserComponent);
-
-    // Set up hot-reloading for development
-#if JUCE_DEBUG
-    webBrowserComponent->goToURL("http://localhost:3000");
-#else
-    // In release, this would load from BinaryData
-    // For now, we'll keep it simple. A resource provider will be added later.
-    webBrowserComponent->goToURL("about:blank");
-#endif
-
-    setSize(400, 300);
+    setSize (400, 300);
 }
 
 VstTestPlaygroundAudioProcessorEditor::~VstTestPlaygroundAudioProcessorEditor()
 {
-    // Important to release resources correctly
-    webBrowserComponent->goToURL("about:blank");
 }
 
-void VstTestPlaygroundAudioProcessorEditor::paint(juce::Graphics& g)
+//==============================================================================
+void VstTestPlaygroundAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 }
 
 void VstTestPlaygroundAudioProcessorEditor::resized()
 {
-    webBrowserComponent->setBounds(getLocalBounds());
+    gainSlider.setBounds (40, 40, 150, 150);
 }
--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,65 +1,164 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
+//==============================================================================
 VstTestPlaygroundAudioProcessor::VstTestPlaygroundAudioProcessor()
     : AudioProcessor(BusesProperties()
-                         .withOutput("Output", juce::AudioChannelSet::stereo(), true)),
+#if !JucePlugin_IsMidiEffect
+#if !JucePlugin_IsSynth
+                         .withInput("Input", juce::AudioChannelSet::stereo(), true)
+#endif
+                         .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+                         ),
       apvts(*this, nullptr, "Parameters", createParameterLayout())
 {
-    // Get atomic pointers to the parameters for the audio thread
-    gainParam = apvts.getRawParameterValue("GAIN");
-    onOffParam = apvts.getRawParameterValue("ONOFF");
-
-    // Set up the oscillator
-    oscillator.setFrequency(440.0f); // A4 note
-    oscillator.initialise([](float x) { return std::sin(x); }); // Sine wave
+    gainParam = apvts.getRawParameterValue("gain");
 }
 
-VstTestPlaygroundAudioProcessor::~VstTestPlaygroundAudioProcessor() = default;
+VstTestPlaygroundAudioProcessor::~VstTestPlaygroundAudioProcessor()
+{
+}
 
+//==============================================================================
 juce::AudioProcessorValueTreeState::ParameterLayout VstTestPlaygroundAudioProcessor::createParameterLayout()
 {
-    std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
+    juce::AudioProcessorValueTreeState::ParameterLayout layout;
 
-    params.push_back(std::make_unique<juce::AudioParameterFloat>("GAIN", "Gain", 0.0f, 1.0f, 0.5f));
-    params.push_back(std::make_unique<juce::AudioParameterBool>("ONOFF", "On/Off", true));
+    layout.add(std::make_unique<juce::AudioParameterFloat>(
+        "gain",
+        "Gain",
+        juce::NormalisableRange<float>(-100.0f, 100.0f, 0.01f),
+        0.0f));
 
-    return { params.begin(), params.end() };
+    return layout;
 }
 
+//==============================================================================
+const juce::String VstTestPlaygroundAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool VstTestPlaygroundAudioProcessor::acceptsMidi() const
+{
+#if JucePlugin_WantsMidiInput
+    return true;
+#else
+    return false;
+#endif
+}
+
+bool VstTestPlaygroundAudioProcessor::producesMidi() const
+{
+#if JucePlugin_ProducesMidiOutput
+    return true;
+#else
+    return false;
+#endif
+}
+
+bool VstTestPlaygroundAudioProcessor::isMidiEffect() const
+{
+#if JucePlugin_IsMidiEffect
+    return true;
+#else
+    return false;
+#endif
+}
+
+double VstTestPlaygroundAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+//==============================================================================
+int VstTestPlaygroundAudioProcessor::getNumPrograms()
+{
+    return 1;
+}
+
+int VstTestPlaygroundAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void VstTestPlaygroundAudioProcessor::setCurrentProgram(int)
+{
+}
+
+const juce::String VstTestPlaygroundAudioProcessor::getProgramName(int)
+{
+    return {};
+}
+
+void VstTestPlaygroundAudioProcessor::changeProgramName(int, const juce::String&)
+{
+}
+
+//==============================================================================
 void VstTestPlaygroundAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
-    juce::dsp::ProcessSpec spec{ sampleRate, (juce::uint32)samplesPerBlock, 2 };
-    oscillator.prepare(spec);
+    juce::dsp::ProcessSpec spec;
+    spec.sampleRate = sampleRate;
+    spec.maximumBlockSize = samplesPerBlock;
+    spec.numChannels = getTotalNumOutputChannels();
+
     gain.prepare(spec);
+    gain.setRampDurationSeconds(0.05);
 }
 
-void VstTestPlaygroundAudioProcessor::releaseResources() {}
+void VstTestPlaygroundAudioProcessor::releaseResources()
+{
+}
 
-void VstTestPlaygroundAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& /*midiMessages*/)
+bool VstTestPlaygroundAudioProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
+{
+#if JucePlugin_IsMidiEffect
+    juce::ignoreUnused(layouts);
+    return true;
+#else
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+        && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+#if !JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+#endif
+
+    return true;
+#endif
+}
+
+void VstTestPlaygroundAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer&)
 {
     juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels = getTotalNumInputChannels();
+
+    auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear(i, 0, buffer.getNumSamples());
+        buffer.clear (i, 0, buffer.getNumSamples());
 
-    // Check the On/Off state. If off, we do nothing and return silence.
-    if (onOffParam->load() < 0.5f)
-        return;
-
-    // Set the gain from our parameter
-    gain.setGainLinear(gainParam->load());
-
-    juce::dsp::AudioBlock<float> block(buffer);
-    juce::dsp::ProcessContextReplacing<float> context(block);
-    
-    // Process the audio
-    oscillator.process(context);
+    juce::dsp::AudioBlock<float> block (buffer);
+    juce::dsp::ProcessContextReplacing<float> context (block);
+    gain.setGainDecibels(gainParam->load());
     gain.process(context);
 }
 
+//==============================================================================
+bool VstTestPlaygroundAudioProcessor::hasEditor() const
+{
+    return true;
+}
+
+juce::AudioProcessorEditor* VstTestPlaygroundAudioProcessor::createEditor()
+{
+    return new VstTestPlaygroundAudioProcessorEditor(*this);
+}
+
+//==============================================================================
 void VstTestPlaygroundAudioProcessor::getStateInformation(juce::MemoryBlock& destData)
 {
     auto state = apvts.copyState();
@@ -70,17 +169,13 @@ void VstTestPlaygroundAudioProcessor::getStateInformation(juce::MemoryBlock& des
 void VstTestPlaygroundAudioProcessor::setStateInformation(const void* data, int sizeInBytes)
 {
     std::unique_ptr<juce::XmlElement> xmlState(getXmlFromBinary(data, sizeInBytes));
-    if (xmlState.get() != nullptr)
+
+    if (xmlState != nullptr)
         if (xmlState->hasTagName(apvts.state.getType()))
             apvts.replaceState(juce::ValueTree::fromXml(*xmlState));
 }
 
-juce::AudioProcessorEditor* VstTestPlaygroundAudioProcessor::createEditor()
-{
-    return new VstTestPlaygroundAudioProcessorEditor(*this);
-}
-
-// This creates new instances of the plugin
+//==============================================================================
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new VstTestPlaygroundAudioProcessor();
